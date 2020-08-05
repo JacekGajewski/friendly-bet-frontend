b1d69011271b5fa4696440ffaf6eefbc1a0f9590
JacekGajewski <jacek.gajewski@ymail.com>
2020-08-05 13:59:30 +0200

    Env variable url set up for heroku backend

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'http://localhost:8080';
+  window.__env.apiUrl = 'https://friendly-bet-backend.herokuapp.com';
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
